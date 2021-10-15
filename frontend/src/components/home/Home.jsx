b3060a694199bb2e3df3e590e06aff47c3cf5802
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2021-10-15 10:41:15 -0300

    feat: creat page home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from '../template/Main'
+
+const Home = (props) => {
+  return(
+    <Main icon="home" title="Início"
+      subtitle="Segundo Projeto do capítulo de React."
+    >
+      <div className='display-4'>Bem Vindo!</div>
+      <hr />
+      <p className="mb-0">Sistema para exemplificar a construção
+        de um cadastro desenvolvido em React!</p>
+    </Main>
+  )
+}
+
+export default Home
