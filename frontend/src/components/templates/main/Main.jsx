ea50589f8a1d304b2054f989b1fc3492fefae3ad
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2021-10-15 10:35:48 -0300

    :recycle: edit Main and add content

--- frontend/src/components/templates/main/Main.jsx
+++ frontend/src/components/templates/main/Main.jsx
@@ -8,9 +8,11 @@ const Main = (props) => {
   return(
     <React.Fragment>
       <Header { ...props }/>
-      <main className="content">
-        Conteúdo
-      </main>
+      <main className="content container-fluid">
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
+        </main>
     </React.Fragment>
   )
 }
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -19,7 +19,14 @@ const App = (props) => {
       icon="home" 
       title="Início" 
       subtitle="Segundo projeto do capítulo de React."
-    />
+    >
+      <div className="display-4">Bem vindo!</div>
+      <hr />
+      <p className="mb-0">
+        Sistema para exemplificar a construção de um 
+        cadastro desenvolvido em React!
+      </p>
+    </Main>
     <Footer />
   </div>
   )
