2d6ea9e70fb9c84328f244d2459ffd972f56aca4
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2021-10-15 09:58:30 -0300

    :recycle: edit footer

--- frontend/src/components/templates/footer/Footer.jsx
+++ frontend/src/components/templates/footer/Footer.jsx
@@ -5,8 +5,11 @@ import './Footer.css'
 const Footer = (props) => {
   return(
     <footer className="footer">
-      Rodapé
-    </footer>
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+      <strong> Milena Alcântara</strong>
+    </span>
+  </footer> 
   )
 }
 
